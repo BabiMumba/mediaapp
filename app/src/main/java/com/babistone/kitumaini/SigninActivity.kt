d4db2218d5b1309765c07e1784607833b50c38d1
Babi Mumba <babimumba243@gmail.com>
2022-08-01 23:22:00 +0200

    add a new file

--- app/src/main/java/com/babistone/kitumaini/SigninActivity.kt
+++ app/src/main/java/com/babistone/kitumaini/SigninActivity.kt
@@ -1,10 +1,14 @@
 package com.babistone.kitumaini
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.babistone.kitumaini.R
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import kotlinx.android.synthetic.main.activity_signin.*
+import org.jetbrains.anko.indeterminateProgressDialog
 import org.jetbrains.anko.startActivityForResult
 
 class SigninActivity : AppCompatActivity() {
@@ -15,7 +19,6 @@ class SigninActivity : AppCompatActivity() {
         .setRequireName(true)
         .build()
     )
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
@@ -26,6 +29,17 @@ class SigninActivity : AppCompatActivity() {
                 .build()
             startActivityForResult(intent,RC_SIGN_IN)
 
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN){
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode== Activity.RESULT_OK){
+                val progresssdialog = indeterminateProgressDialog("setting up your account")
+
+            }
         }
     }
 }
