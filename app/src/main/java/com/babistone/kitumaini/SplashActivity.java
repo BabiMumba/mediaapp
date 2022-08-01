0295d22c42102d7136dcd788010f4d3f5a4057fc
Babi Mumba <babimumba243@gmail.com>
2022-08-01 23:42:01 +0200

    add  a new acivity

---
+++ app/src/main/java/com/babistone/kitumaini/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.babistone.kitumaini;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+}
