02fa794a393ece4234072ebaad8b4469bb5320ab
Babi Mumba <babimumba243@gmail.com>
2022-08-02 10:04:54 +0200

    add anew file

--- app/src/main/java/com/babistone/kitumaini/model/util/Storage.kt
+++ app/src/main/java/com/babistone/kitumaini/model/util/Storage.kt
@@ -4,10 +4,17 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import java.util.*
 
 object Storage {
     private val storageInstance: FirebaseStorage by lazy {  FirebaseStorage.getInstance()  }
     private val currentUserref: StorageReference
     get() = storageInstance.reference
         .child(FirebaseAuth.getInstance().uid ?: throw NullPointerException("UID is null"))
+
+    fun unploadProfilPhoto(imagebyte:ByteArray,
+    onsucces:(imagepath:String) -> Unit
+                           ){
+        val ref = currentUserref.child("profilPicture/${UUID.nameUUIDFromBytes(imagebyte)}")
+    }
 }
