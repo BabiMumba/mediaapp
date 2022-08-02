772f7ae6033190d74b9ce999efd69c941df37267
Babi Mumba <babimumba243@gmail.com>
2022-08-02 09:05:06 +0200

    add a new file

--- app/src/main/java/com/babistone/kitumaini/model/util/FirestoreUtil.kt
+++ app/src/main/java/com/babistone/kitumaini/model/util/FirestoreUtil.kt
@@ -1,7 +1,14 @@
 package com.babistone.kitumaini.model.util
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 
 object FirestoreUtil {
     private val firestoreinstance :FirebaseFirestore by lazy { FirebaseFirestore.getInstance() }
+
+    private val currentUserDocRef: DocumentReference
+    get() = firestoreinstance.document("usera/${FirebaseAuth.getInstance().uid
+    
+        ?:throw NullPointerException("UID is null.")}")
 }
