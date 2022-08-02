b20afa0d25fb973fcf4e86570abc00793b5839a2
Babi Mumba <babimumba243@gmail.com>
2022-08-02 10:22:41 +0200

    add a new file

---
+++ app/src/main/java/com/babistone/kitumaini/glide/FirebaseglideModule.kt
@@ -0,0 +1,9 @@
+package com.babistone.kitumaini.glide
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class FirebaseglideModule : AppGlideModule(){
+
+}
