8ee518378a9a0c50d1764575960f32489c525b3b
Babi Mumba <babimumba243@gmail.com>
2022-08-02 10:00:28 +0200

    add a new file

--- app/src/main/java/com/babistone/kitumaini/model/util/Storage.kt
+++ app/src/main/java/com/babistone/kitumaini/model/util/Storage.kt
@@ -1,4 +1,13 @@
 package com.babistone.kitumaini.model.util
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+
 object Storage {
+    private val storageInstance: FirebaseStorage by lazy {  FirebaseStorage.getInstance()  }
+    private val currentUserref: StorageReference
+    get() = storageInstance.reference
+        .child(FirebaseAuth.getInstance().uid ?: throw NullPointerException("UID is null"))
 }
