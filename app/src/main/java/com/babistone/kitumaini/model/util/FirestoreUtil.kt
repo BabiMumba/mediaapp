aba0329356bb811e4bfb3b940025b3e4c1ae1037
Babi Mumba <babimumba243@gmail.com>
2022-08-02 09:16:17 +0200

    add a new file

--- app/src/main/java/com/babistone/kitumaini/model/util/FirestoreUtil.kt
+++ app/src/main/java/com/babistone/kitumaini/model/util/FirestoreUtil.kt
@@ -1,5 +1,6 @@
 package com.babistone.kitumaini.model.util
 
+import com.babistone.kitumaini.model.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.DocumentSnapshot
@@ -18,8 +19,22 @@ object FirestoreUtil {
     fun initCurrentUserIffirstTime(onComplet: () -> Unit){
         currentUserDocRef.get().addOnSuccessListener { documentsnapshot ->
             if (!documentsnapshot.exists()){
+                val newUser = User(FirebaseAuth.getInstance().currentUser?.displayName?: "","",null)
+                currentUserDocRef.set(newUser).addOnSuccessListener {
+                    onComplet()
+                }
 
+            }else{
+                onComplet
             }
         }
+    }
+    fun updateCurrentUser(name:String="",bio:String="",profilPicturepath:String?=null){
+        val userFielMap = mutableMapOf<String,Any>()
+        if (name.isBlank()) userFielMap["name"] = name
+        if (bio.isBlank())  userFielMap["bio"] = bio
+        if (profilPicturepath != null)
+            userFielMap["profilPicturePath"] = profilPicturepath
+        currentUserDocRef.update(userFielMap)
     }
 }
